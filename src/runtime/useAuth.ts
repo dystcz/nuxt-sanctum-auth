87c5ec2cdec145f27053d873e5efe8d64a578b76
Paul Cross <me@paulcross.cz>
2023-07-26 14:37:35 +0200

    add TSDoc

--- src/runtime/useAuth.ts
+++ src/runtime/useAuth.ts
@@ -13,6 +13,17 @@ export function useAuth() {
     }
   })
 
+  /**
+   * Method to get CSRF token which will be automatically
+   * set and used in header of all requests
+   * @returns ```ts
+   * Promise<void>
+   * ```
+   * @example
+   * ```ts
+   * await csrf()
+   * ```
+   * */
   async function csrf(): Csrf {
     await ofetch(config.endpoints.csrf, {
       baseURL: config.baseUrl,
@@ -24,6 +35,24 @@ export function useAuth() {
     })
   }
 
+  /**
+   * Method to make requests to API
+   * @param endpoint - endpoint to make request to
+   * @param options - options for request
+   * @returns ```ts
+   * Promise<FetchResponse<Any>>
+   * ```
+   * @example
+   * ```ts
+   * // using in script setup
+   * const { apiFetch } = useAuth()
+   * const { data, error } = await useAsyncData(
+   *  'posts',
+   *  () => apiFetch('posts', { method: 'GET' }),
+   *  { initialData: [] }
+   * )
+   * ```
+   * */
   function apiFetch(endpoint: FetchRequest, options?: FetchOptions) {
     const fetch = ofetch.create({
       baseURL: config.baseUrl,
@@ -38,22 +67,68 @@ export function useAuth() {
       } as HeadersInit
     })
 
-    return fetch.raw(endpoint, options)
+    return fetch(endpoint, options)
   }
+
+  /**
+   * Method to get CSRF token from cookie
+   * @returns ```ts
+   * string | null
+   * ```
+   * @example
+   * ```ts
+   * const { getToken } = useAuth()
+   * const token = getToken()
+   * ```
+   * */
 
   function getToken(): string | null {
     auth.value.token = useCookie(config.csrf.tokenCookieKey)?.value || null
     return auth.value.token
   }
 
+  /**
+   * Method to set CSRF token to cookie
+   * @param token - CSRF token
+   * @returns ```ts
+   * void
+   * ```
+   * @example
+   * ```ts
+   * const { setToken } = useAuth()
+   * setToken('token')
+   * ```
+   * */
   function setToken(token: string): void {
     useCookie(config.csrf.tokenCookieKey).value = token
   }
 
+  /**
+   * Method to clear CSRF token from cookie
+   * @returns ```ts
+   * void
+   * ```
+   * @example
+   * ```ts
+   * const { clearToken } = useAuth()
+   * clearToken()
+   * ```
+   * */
   function clearToken(): void {
     useCookie(config.csrf.tokenCookieKey).value = null
   }
 
+  /**
+   * Method to clear auth state
+   * @returns ```ts
+   * void
+   * ```
+   * @example
+   * ```ts
+   * const { clearState } = useAuth()
+   * clearState()
+   * ```
+   * */
   function clearState(): void {
     auth.value.user = null
     auth.value.loggedIn = false
@@ -61,11 +136,46 @@ export function useAuth() {
     clearToken()
   }
 
+  /**
+   * Method to set user to auth state
+   * @param user - user object
+   * @returns ```ts
+   * void
+   * ```
+   * @example
+   * ```ts
+   * const { setUser } = useAuth()
+   * setUser({ name: 'John Doe' })
+   * ```
+   * */
   function setUser(user: any) {
     auth.value.user = user
     auth.value.loggedIn = true
   }
 
+  /**
+   * Method to get user from API and set it to auth state
+   * @typeParam ResponseT - type of server response
+   * @typeParam ErrorT - type of server error
+   * @returns ```ts
+   * Promise<{
+   *   response: ResponseT
+   *   error: ErrorT
+   * }>
+   * ```
+   * @example
+   * ```ts
+   * const { response, error } = await getUser<ResponseT, ErrorT>()
+   * if (error) {
+   *   // handle errors
+   *   console.log(error)
+   * }
+   * if (response) {
+   *   // handle response (set to local variable, etc...)
+   *   console.log(response)
+   * }
+   * ```
+   * */
   async function getUser<ResponseT, ErrorT>(): Response<ResponseT, ErrorT> {
     try {
       const response = await apiFetch(config.endpoints.user)
@@ -79,6 +189,33 @@ export function useAuth() {
     }
   }
 
+  /**
+   * Method to make login request
+   * @param data - object with user credentials
+   * @typeParam ResponseT - type of server response
+   * @typeParam ErrorT - type of server error
+   * @returns ```ts
+   * Promise<{
+   *   response: ResponseT
+   *   error: ErrorT
+   * }>
+   * ```
+   * @example
+   * ```ts
+   * const { response, error } = await loginRequest<ResponseT, ErrorT>({
+   *   email: 'email@example.com',
+   *   password: 'password'
+   * })
+   * if (error) {
+   *   // handle errors
+   *   console.log(error)
+   * }
+   * if (response) {
+   *   // handle response (manual redirect, etc...)
+   *   console.log(response)
+   * }
+   * ```
+   * */
   async function loginRequest<ResponseT, ErrorT>(
     data: any
   ): Response<ResponseT, ErrorT> {
@@ -110,6 +247,36 @@ export function useAuth() {
     }
   }
 
+  /**
+   * Method to login user
+   * @param data - object with user credentials
+   * @typeParam ResponseT - type of server response
+   * @typeParam ErrorT - type of server error
+   * @returns ```ts
+   * // if redirectByDefault is true (default)
+   * // redirects to specified home page after successful login
+   * // otherwise returns response and error
+   * Promise<{
+   *   response: ResponseT
+   *   error: ErrorT
+   * }>
+   * ```
+   * @example
+   * ```ts
+   * const { response, error } = await login<ResponseT, ErrorT>({
+   *   email: 'email@example.com',
+   *   password: 'password'
+   * })
+   * if (error) {
+   *   // handle errors
+   *   console.log(error)
+   * }
+   * if (response) {
+   *   // handle response (manual redirect, etc...)
+   *   console.log(response)
+   * }
+   * ```
+   * */
   async function login<ResponseT = unknown, ErrorT = unknown>(
     data: any
   ): Response<ResponseT, ErrorT> {
@@ -126,6 +293,32 @@ export function useAuth() {
     return await getUser<ResponseT, ErrorT>()
   }
 
+  /**
+   * Method to logout user
+   * @typeParam ResponseT - type of server response
+   * @typeParam ErrorT - type of server error
+   * @returns ```ts
+   * // if redirectByDefault is true (default)
+   * // redirects to specified login page after successful logout
+   * // otherwise returns response and error
+   * {
+   *   response: ResponseT
+   *   error: ErrorT
+   * }
+   * ```
+   * @example
+   * ```ts
+   * const { response, error } = await logout<ResponseT, ErrorT>()
+   * if (error) {
+   *   // handle errors
+   *   console.log(error)
+   * }
+   * if (response) {
+   *   // handle response (manual redirect, etc...)
+   *   console.log(response)
+   * }
+   * ```
+   * */
   async function logout<ResponseT, ErrorT>(): Response<ResponseT, ErrorT> {
     try {
       const response = await apiFetch(config.endpoints.logout, {
@@ -143,15 +336,7 @@ export function useAuth() {
       }
     }
   }
-  async function checkAuth(): Promise<boolean> {
-    if (auth.value.loggedIn === true) return true
 
-    if (config.token) {
-      getToken()
-    }
-    await getUser()
-    return auth.value.loggedIn
-  }
   return {
     auth,
     apiFetch,
@@ -163,7 +348,6 @@ export function useAuth() {
     setToken,
     clearToken,
     clearState,
-    setUser,
-    checkAuth
+    setUser
   }
 }
